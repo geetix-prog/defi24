5a79949c37a7c472a0f2d06e8d80445ec7b4bc7e
Mathis <mathis.guellati@gmail.com>
2025-12-10 00:00:58 +0100

    fix:pbRedirect

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -1,6 +1,6 @@
 import PocketBase from "pocketbase";
 import type { TypedPocketBase } from "./pocketbase-types";
 
-const pb = new PocketBase("http://37.59.112.36:8091") as TypedPocketBase;
+const pb = new PocketBase("http://defi24.mathis-guellati.fr:8091") as TypedPocketBase;
 
 export default pb;
