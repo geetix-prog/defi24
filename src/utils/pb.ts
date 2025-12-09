503eab366f9b4c253e180ed93b38a95750ad5766
Mathis <mathis.guellati@gmail.com>
2025-12-10 00:11:36 +0100

    fix:testImg

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -1,6 +1,16 @@
 import PocketBase from "pocketbase";
 import type { TypedPocketBase } from "./pocketbase-types";
 
-const pb = new PocketBase("http://defi24.mathis-guellati.fr:8091") as TypedPocketBase;
+const getBaseUrl = () => {
+    // En développement local
+    if (import.meta.env.DEV) {
+        return "http://127.0.0.1:8090";
+    }
+    
+    // En production, utilise l'URL Netlify qui redirigera vers PocketBase
+    return "/api";
+};
+
+const pb = new PocketBase(getBaseUrl()) as TypedPocketBase;
 
 export default pb;
