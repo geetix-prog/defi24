2c629cbdd0162981ecdfdfc2ea19e2199b6ed234
Mathis <mathis.guellati@gmail.com>
2025-12-10 00:29:52 +0100

    fix:pocketbaseSecure

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -1,6 +1,6 @@
 import PocketBase from "pocketbase";
 import type { TypedPocketBase } from "./pocketbase-types";
 
-const pb = new PocketBase("http://defi24.mathis-guellati.fr:8091") as TypedPocketBase;
+const pb = new PocketBase("https://defi24.mathis-guellati.fr") as TypedPocketBase;
 
 export default pb;
